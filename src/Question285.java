9aaad4ba1e3005126ecc2305fd7b0b0e03b3c201
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-05 22:37:22 -0700

    Add Question285

---
+++ src/Question285.java
@@ -0,0 +1,16 @@
+import common.TreeNode;
+
+public class Question285 {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        while (root != null) {
+            if (p.val < root.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return successor;
+    }
+}
