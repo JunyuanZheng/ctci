c6ddda629c597238eba1aff94ded35a59479fb03
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-06 10:57:52 -0700

    Add Question 295

---
+++ src/Question295.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Question295 {
+    /*
+    class MedianFinder {
+        ArrayList<Integer> list;
+
+        public MedianFinder() {
+            this.list = new ArrayList<>();
+        }
+
+        public void addNum(int num) {
+            this.list.add(num);
+        }
+
+        public double findMedian() {
+            Collections.sort(list);
+            int size = list.size();
+            return size % 2 != 0 ? list.get(size / 2) : (double) (list.get(size / 2 - 1) + list.get(size / 2)) / 2;
+        }
+    }
+    */
+}
