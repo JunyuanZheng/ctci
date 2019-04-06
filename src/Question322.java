62d7dd92195c523dad7e98331cf22c7a9d0ab14b
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-06 12:46:43 -0700

    Add Question322

---
+++ src/Question322.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Question322 {
+    public int coinChange(int[] coins, int amount) {
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+        for (int i = 1; i <= amount; i += 1) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && memo[i - coin] != Integer.MAX_VALUE) {
+                    memo[i] = Math.min(memo[i], memo[i - coin] + 1);
+                }
+            }
+        }
+        return memo[amount] == Integer.MAX_VALUE ? -1 : memo[amount];
+    }
+}
