0b1bcc938ba3c031278f8274cb0683f3c87b1cc7
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-08 21:04:13 -0700

    Add Question134

---
+++ src/Question134.java
@@ -0,0 +1,14 @@
+public class Question134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, start = 0, tank = 0;
+        for (int i = 0; i < gas.length; i += 1) {
+            tank = tank + gas[i] - cost[i];
+            total = total + gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+        return total >= 0 ? start : -1;
+    }
+}
