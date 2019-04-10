aeb6b495adf1f2c2c7eb2b7f4859fc4aab4da5a9
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-09 20:44:06 -0700

    Add Question189

---
+++ src/Question189.java
@@ -0,0 +1,20 @@
+public class Question189 {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
