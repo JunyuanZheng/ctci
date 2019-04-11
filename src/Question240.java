67e4e0552208a83870e10573a5895a52bb0472b6
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-10 18:45:19 -0700

    Add Question240

--- src/Question240.java
+++ src/Question240.java
@@ -1,10 +1,10 @@
 public class Question240 {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0)
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
             return false;
-        int i = 0;
-        int j = matrix[0].length - 1;
-        while (i <= matrix.length - 1 && j >= 0) {
+        int height = matrix.length, width = matrix[0].length;
+        int i = 0, j = width - 1;
+        while (i < height && j >= 0) {
             int value = matrix[i][j];
             if (value == target)
                 return true;
