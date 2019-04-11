b21b3bd5fa4b907106b0516f434355c7cd1df5f0
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-10 18:15:22 -0700

    Add Question236

---
+++ src/Question236.java
@@ -0,0 +1,16 @@
+import common.TreeNode;
+
+public class Question236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+            return root;
+        else if (left != null ^ right != null)
+            return left != null ? left : right;
+        else
+            return null;
+    }
+}
