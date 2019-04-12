77cdcf6b2e5da0410e44e67277e70ef1e1572c81
zjunyuan <zheng.junyuan@move.com>
2019-04-12 15:11:16 -0700

    Add Question21

---
+++ src/Question21.java
@@ -0,0 +1,20 @@
+import common.ListNode;
+
+public class Question21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode ptr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                ptr.next = l1;
+                l1 = l1.next;
+            } else {
+                ptr.next = l2;
+                l2 = l2.next;
+            }
+            ptr = ptr.next;
+        }
+        ptr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
