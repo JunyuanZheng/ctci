72aae04a4f935a078d7b34aff4e5cc1bfe0fdb4e
zjunyuan <zheng.junyuan@move.com>
2019-04-12 15:07:06 -0700

    Update Question41

--- src/Question41.java
+++ src/Question41.java
@@ -2,21 +2,21 @@ public class Question41 {
     public int firstMissingPositive(int[] nums) {
         for (int i = 0; i < nums.length;) {
             if (nums[i] == i + 1 || nums[i] <= 0 || nums[i] > nums.length)
-                i+= 1;
+                i += 1;
             else if (nums[nums[i] - 1] != nums[i])
-                swap(nums, i, nums[i] - 1);
+                swap(nums, nums[i] - 1, i);
             else
                 i += 1;
         }
         int i = 0;
-        while (i < nums.length && nums[i] == i + 1)
+        while (i < nums.length && i + 1 == nums[i])
             i += 1;
         return i + 1;
     }
 
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
+        nums[j] = tmp;
     }
 }
