5c802838294fc99a4bbcacda0d2235c672061273
zjunyuan <zheng.junyuan@move.com>
2019-04-12 12:54:58 -0700

    Add Question14

---
+++ src/Question14.java
@@ -0,0 +1,12 @@
+public class Question14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i += 1) {
+            while (!strs[i].startsWith(result))
+                result = result.substring(0, result.length() - 1);
+        }
+        return result;
+    }
+}
