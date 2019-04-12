e9dea57d1f298efda3a2572a6bb300b91e5fc0c6
zjunyuan <zheng.junyuan@move.com>
2019-04-12 15:19:18 -0700

    Add Question19

---
+++ src/Question19.java
@@ -0,0 +1,20 @@
+import common.ListNode;
+
+public class Question19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n -= 1;
+        }
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
