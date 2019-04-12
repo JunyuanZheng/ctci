a3aade5c9536ae2fa3be7a27a9b77484cea7b038
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-11 21:06:37 -0700

    Add Question153

---
+++ src/Question153.java
@@ -0,0 +1,15 @@
+public class Question153 {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high])
+                low = mid + 1;
+            else if (nums[mid] <= nums[high])
+                high = mid;
+        }
+        return nums[low];
+    }
+}
