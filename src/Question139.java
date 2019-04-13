f1c23c533ee7611083aff04cf176ba38fc548918
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-12 21:29:43 -0700

    Update Questiion139

--- src/Question139.java
+++ src/Question139.java
@@ -5,7 +5,7 @@ public class Question139 {
         boolean[] result = new boolean[s.length() + 1];
         result[0] = true;
         for (int i = 1; i <= s.length(); i += 1) {
-            for (int j = 0; j < i; j += 1) {
+            for (int j = 0; j <= i; j += 1) {
                 if (result[j] && wordDict.contains(s.substring(j, i))) {
                     result[i] = true;
                     break;
