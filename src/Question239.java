9a94ed2137413ac1a8ebb57ec6a98763e7409aaa
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-15 16:59:22 -0700

    Update Question239

--- src/Question239.java
+++ src/Question239.java
@@ -1,23 +1,18 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 public class Question239 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length == 0)
             return new int[] {};
-        int size = nums.length;
-        int[] result = new int[size - k + 1];
-        int ri = 0;
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i += 1) {
-            while (!deque.isEmpty() && deque.peek() < i - k + 1)
-                deque.poll();
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i])
-                deque.pollLast();
-            deque.offer(i);
-            if (i >= k - 1) {
-                result[ri++] = nums[deque.peek()];
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i <= nums.length - k; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < k; j += 1) {
+                max = Math.max(max, nums[i + j]);
             }
+            result[i] = max;
         }
         return result;
     }
