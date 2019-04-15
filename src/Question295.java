398dc031cf123c2c5fc3b9c5bab36fe263b4a8fa
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-14 21:58:52 -0700

    Update Question295

--- src/Question295.java
+++ src/Question295.java
@@ -21,4 +21,36 @@ public class Question295 {
         }
     }
     */
+
+    public static class MedianFinder {
+        ArrayList<Integer> list;
+
+        public MedianFinder() {
+            list = new ArrayList<>();
+        }
+
+        public void addNum(int num) {
+            int low = 0, high = list.size() - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (list.get(mid) == num) {
+                    list.add(mid, num);
+                    return;
+                } else if (list.get(mid) < num) {
+                    low = mid + 1;
+                } else
+                    high = mid - 1;
+            }
+            list.add(low, num);
+        }
+
+        public double findMedian() {
+            int size = list.size();
+            if (size % 2 == 1) {
+                return list.get(size / 2);
+            } else {
+                return (list.get(size / 2 - 1) + list.get(size / 2)) / 2.0;
+            }
+        }
+    }
 }
