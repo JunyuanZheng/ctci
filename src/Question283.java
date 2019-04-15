0adc1afb2f4aa97165034437784cf9fe4ff098b6
Junyuan Zheng <joseph.zjy@gmail.com>
2019-04-15 09:56:42 -0700

    Update Question283

--- src/Question283.java
+++ src/Question283.java
@@ -5,9 +5,9 @@ public class Question283 {
         int idx = 0;
         for (int i = 0; i < nums.length; i += 1) {
             if (nums[i] != 0) {
-                int tmp = nums[i];
-                nums[i] = 0;
-                nums[idx] = tmp;
+                int tmp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = tmp;
                 idx += 1;
             }
         }
